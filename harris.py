5d5ecc7d883cc749bdac76e66c5109d3529778d9
Mamun Hasan <mamunthecoder@gmail.com>
2020-12-08 01:30:19 +0600

    # Info - response model update

--- harris.py
+++ harris.py
@@ -3,20 +3,20 @@ import csv
 import cv2
 import glob
 import numpy as np
-import pandas as pd
 from matplotlib import pyplot as plt
 
 
-def dnorm(x, mu, sd):
-    return 1 / (np.sqrt(2 * np.pi) * sd) * np.e ** (-np.power((x - mu) / sd, 2) / 2)
+def dnorm(x, sd):
+    return 1 / (np.sqrt(2 * np.pi) * sd) * np.e ** (-np.power(x / sd, 2) / 2)
 
 
 def myGaussianKernel(size, sigma=1, verbose=False):
     kernel_1D = np.linspace(-(size // 2), size // 2, size)
     for i in range(size):
-        kernel_1D[i] = dnorm(kernel_1D[i], 0, sigma)
-    kernel_2D = np.outer(kernel_1D.T, kernel_1D.T)
+        kernel_1D[i] = dnorm(kernel_1D[i], sigma)
+    print(kernel_1D)
 
+    kernel_2D = np.outer(kernel_1D, kernel_1D)
     kernel_2D *= 1.0 / kernel_2D.max()
 
     if verbose:
@@ -28,8 +28,8 @@ def myGaussianKernel(size, sigma=1, verbose=False):
 
 
 def myImageFilter(image, kernel, average=False, verbose=False):
-    # print("Image Shape : {}".format(image.shape))
-    # print("Kernel Shape : {}".format(kernel.shape))
+    print("Image Shape : {}".format(image.shape))
+    print("Kernel Shape : {}".format(kernel.shape))
 
     if verbose:
         plt.imshow(image, cmap='gray')
@@ -59,7 +59,7 @@ def myImageFilter(image, kernel, average=False, verbose=False):
             if average:
                 output[row, col] /= kernel.shape[0] * kernel.shape[1]
 
-    print("Output Image Size: {0}".format(output.shape))
+    print("Output Image Size : {}".format(output.shape))
 
     if verbose:
         plt.imshow(output, cmap='gray')
@@ -69,22 +69,21 @@ def myImageFilter(image, kernel, average=False, verbose=False):
     return output
 
 
-def findImageCorners(gray_img, kernel, verbose=False):
+def findImageCorners(gray_img, kernel, method, verbose=False):
     k = 0.04
     window_size = 5
-    threshold = 10000.00
 
-    # dy, dx = np.gradient(gray_img) # skip builtin method
+    # Gx, Gy = np.gradient(gray_img) # skip builtin method
     Gxy = myImageFilter(gray_img, kernel, average=True)
-    dy = np.diff(Gxy, axis=0, append=0)
+    Gy = np.diff(Gxy, axis=0, append=0)
     if verbose:
         plt.title("y derivative image")
-        plt.imshow(dy, cmap='gray')
+        plt.imshow(Gy, cmap='gray')
         plt.show()
-    dx = np.diff(Gxy, axis=1, append=0)
+    Gx = np.diff(Gxy, axis=1, append=0)
     if verbose:
         plt.title("x derivative image")
-        plt.imshow(dx, cmap='gray')
+        plt.imshow(Gx, cmap='gray')
         plt.show()
 
     offset = int(window_size / 2)
@@ -92,12 +91,12 @@ def findImageCorners(gray_img, kernel, verbose=False):
     x_range = gray_img.shape[1] - offset
 
     # print("Gxy Shape : {}".format(Gxy.shape))
-    # print("dy Shape : {}".format(dy.shape))
-    # print("dx Shape : {}".format(dx.shape))
+    print("Gx Shape : {}".format(Gx.shape))
+    print("Gy Shape : {}".format(Gy.shape))
 
-    Ixx = dx ** 2
-    Ixy = dy * dx
-    Iyy = dy ** 2
+    Ixx = Gx ** 2
+    Ixy = Gy * Gx
+    Iyy = Gy ** 2
 
     corner_list = []
     output_img = cv2.cvtColor(gray_img.copy(), cv2.COLOR_GRAY2RGB)
@@ -120,19 +119,36 @@ def findImageCorners(gray_img, kernel, verbose=False):
             Sxy = windowIxy.sum()
             Syy = windowIyy.sum()
 
+            M = np.array([[Sxx, Sxy], [Sxy, Syy]])
+
             # Calculate determinant and trace of the matrix
             det = (Sxx * Syy) - (Sxy ** 2)
             trace = Sxx + Syy
 
-            # Calculate r for Harris Corner equation
-            r = det - k * (trace ** 2)
+            if method == "k":
+                # Calculate r for Kanade & Tomasi Corner equation
+                # lamda1 * lamda2 = det
+                # lamda1 + lamda2 = trace
+                w, v = np.linalg.eig(M)
+                r = np.min(w)
+                threshold = 1.00
+            elif method == "n":
+                # Calculate r for Nobel Corner equation
+                e = 1
+                r = det / (trace + e)
+                threshold = 1.00
+            else:
+                # Calculate r for Harris Corner equation
+                r = det - k * (trace ** 2)
+                threshold = 10000.00
 
             if r > threshold:
                 corner_list.append([x, y, r])
+                # cv2.circle(output_img, (x, y), 1, 255, -1)
                 output_img[y, x] = (0, 0, 255)
 
     if verbose:
-        plt.title("x derivative image")
+        plt.title("Output Image")
         plt.imshow(output_img, cmap='gray')
         plt.show()
 
@@ -140,21 +156,15 @@ def findImageCorners(gray_img, kernel, verbose=False):
 
 
 def saveCornerResult(filename, corner_list, corner_img):
-    if corner_list:
-        csvfile = os.path.join("output", f"{filename}.csv")
-        with open(csvfile, 'w') as corner_file:
-            writer = csv.DictWriter(corner_file, fieldnames=["x", "y", "r"])
-            writer.writeheader()
-            for i in range(len(corner_list)):
-                writer.writerow({
-                    "x": str(corner_list[i][0]),
-                    "y": str(corner_list[i][1]),
-                    "r": str(corner_list[i][2])
-                })
-        df = pd.read_csv(csvfile)
-        print(filename, 'corners', len(df.index))
-        plt.bar(df['x'], df['r'])
-        plt.savefig(os.path.join("output", "charts", f"{filename}"))
+    with open(os.path.join("output", "charts", f"{filename}.csv"), 'w') as corner_file:
+        writer = csv.DictWriter(corner_file, fieldnames=["x", "y", "r"])
+        writer.writeheader()
+        for i in range(len(corner_list)):
+            writer.writerow({
+                "x": str(corner_list[i][0]),
+                "y": str(corner_list[i][1]),
+                "r": str(corner_list[i][2])
+            })
 
     if corner_img is not None:
         cv2.imwrite(os.path.join("output", "images", filename), corner_img)
@@ -172,7 +182,7 @@ def main():
         except Exception as error:
             print(f"Input {filename}: {error}")
             continue
-        corner_list, output_img = findImageCorners(gray_img, kernel)
+        corner_list, output_img = findImageCorners(gray_img, kernel, method="n")
         saveCornerResult(filename, corner_list, output_img)
 
 
---
+++ report.py
@@ -0,0 +1,18 @@
+import os
+import glob
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def main():
+    files = glob.glob(os.path.join("images", "*.csv"))
+    for file in files:
+        filename = file.split(os.sep)[-1]
+        df = pd.read_csv(file)
+        print(filename, 'corners', len(df.index))
+        plt.bar(df['x'], df['r'])
+        plt.savefig(os.path.join("output", "charts", f"{filename}"))
+
+
+if __name__ == "__main__":
+    main()
